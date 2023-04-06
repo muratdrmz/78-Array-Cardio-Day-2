0f4d4ad791b4a6c97fcfbc1b72dbea61d479b1dc
muratdrmz <mrtdrmz@windowslive.com>
2023-04-06 19:03:07 +0200

    end

--- script.js
+++ script.js
@@ -27,12 +27,12 @@
      );
 
 
-    console.log({isAdult});
+    // console.log({isAdult});
     // Array.prototype.every() // is everyone 19 or older?
      const allAdult = people.every(
        (person) => new Date().getFullYear() - person.year >= 19
      );
-     console.log({allAdult});
+    //  console.log({allAdult});
 
     // Array.prototype.find()
     // Find is like filter, but instead returns just the one you are looking for
@@ -44,19 +44,71 @@
     //  }
     // })
     const comment=comments.find(comment=>comment.id===823423)
-    console.log(comment);
+    // console.log(comment);
 
     // Array.prototype.findIndex()
     // Find the comment with this ID
     // delete the comment with the ID of 823423
 
     const index = comments.findIndex((comment) => comment.id === 823423);
-    console.log(index);
+    // console.log(index);
     comments.splice(index,1)
-    console.table(comments);
+    // console.table(comments);
     
     const newComments=[
      ...comments.slice(0,index),
      ...comments.slice(index+1)
     ]
-    console.table(newComments);
+    // console.table(newComments);
+
+    // HTML 5 CANVAS
+
+    const canvas=document.querySelector('#draw');
+    const ctx=canvas.getContext('2d');
+    canvas.width=window.innerWidth;
+    canvas.hight=window.innerHeight;
+    ctx.strokeStyle='#BADASS';
+    ctx.lineJoin='round';
+    ctx.lineCap='round';
+    ctx.lineWidth=100;
+
+    let isDrawing=false;
+    let lastX=0;
+    let lastY=0;
+    let hue=0;
+    let direction=true;
+
+    function draw(e){
+      if(!isDrawing) return // stop the fn from running when no moused down
+      console.log(e);
+      ctx.strokeStyle=`hsl(${hue},100%,50%)`;
+      ctx.beginPath();
+      // start from
+      ctx.moveTo(lastX,lastY);
+      // go to
+      ctx.lineTo(e.offsetX,e.offsetY);
+      ctx.stroke();
+      [lastX,lastY] = [e.offsetX,e.offsetY];
+
+      hue++;
+      if(hue>360){
+        hue=0;
+      }
+      if(ctx.lineWidth>=100||ctx.lineWidth<=1){
+        direction=!direction
+      }
+      if(direction){
+        ctx.lineWidth++;
+      }else{
+        ctx.lineWidth--
+      }
+    }
+
+    canvas.addEventListener('mousedown',(e)=>{
+      isDrawing=true;
+      [lastX, lastY] = [e.offsetX, e.offsetY];
+    });
+    canvas.addEventListener("mousemove", draw);
+    canvas.addEventListener("mouseup", () => isDrawing = false);
+    canvas.addEventListener('mouseout',()=>isDrawing=false)
+
